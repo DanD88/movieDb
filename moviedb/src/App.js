cfb893385f60adf5980fe94ae5183c2f95941a20
Daniel Davidson <DanDavidson88@gmail.com>
2022-05-09 10:05:45 -0700

    added

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchMovie from "./components/searchMovie"
+import Footer from "./components/footer"
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="container">
+          <h1 className="title">Movie Search</h1>
+          <SearchMovie />
+          {/* <Footer /> */}
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
